863507da81d59d1f1284624eb4582ad2217390d7
HidekiHiguchi <hideki-higuchi@outlook.com>
2015-02-26 00:49:07 -0300

    fix

--- TecWeb_2/atividade1.php
+++ TecWeb_2/atividade1.php
@@ -1,13 +1,8 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+<!DOCTYPE HTML>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Atividade1</title>
     </head>
     <body>
         <?php
@@ -25,22 +20,28 @@ and open the template in the editor.
             $MI = (30*$P1 + 10*$M1 + 10*$M2 + 0.5*$Proj1 + 0.5*$Proj3 + 
             0.5*$Proj4 + 10*$TrabF + 20*$Proc)/100;
             
-            if($M1 >= 7.5){
-                $MF = $MI;
-                echo 'Aprovado sem Prova Final';
-                echo 'Parabéns!!! Boas férias';
-            }else{
-                $MF = ($MI + $PF)/2;
-                if($MF>=6.0){
-                    echo 'Aprovado com Prova Final';
-                    echo 'Agora já pode descansar tranquilo.';
-                }else{
-                    echo 'Reprovado';
-                    echo 'Não foi dessa vez. Ano que vem tem mais TWII.';
+		if($M1 >= 7.5)
+		{
+                	$MF = $MI;
+                	echo 'Aprovado sem Prova Final';
+			echo 'Parabéns!!! Boas férias';
+		}
+		else
+		{
+                	$MF = ($MI + $PF)/2;
+                	if($MF>=6.0)
+		{
+			echo 'Aprovado com Prova Final';
+			echo 'Agora já pode descansar tranquilo.';
+                }
+		else
+		{
+			echo 'Reprovado';
+			echo 'Não foi dessa vez. Ano que vem tem mais TWII.';
                 }
             }
-            echo "Média Intermediária: $MI";
-            echo "Média Final: $MF";
+			echo "Média Intermediária: $MI";
+			echo "Média Final: $MF";
            
         ?>
     </body>
